320c8c20a5ff2dd154b0eddb12184bb80844eaf1
Kubijessa <kubilay.jessa@gmail.com>
2023-11-16 14:59:05 +0100

    modified:   app.py 	new file:   docs/README.md 	new file:   docs/_config.yml 	new file:   docs/assets/images/fswd-intro_00.png 	new file:   docs/assets/images/fswd-intro_02.png 	new file:   docs/design-decisions/design-decisions.md 	new file:   docs/index.md 	new file:   docs/team-eval/contributions.md 	new file:   docs/team-eval/goals.md 	new file:   docs/team-eval/improvements.md 	new file:   docs/team-eval/index.md 	new file:   docs/team-eval/peer-review.md 	new file:   docs/technical-docs/app-behavior.md 	new file:   docs/technical-docs/app-structure.md 	new file:   docs/technical-docs/data-model.md 	new file:   docs/technical-docs/index.md 	new file:   docs/technical-docs/reference.md 	new file:   docs/ui-components.md 	new file:   docs/user-eval/user-eval.md 	new file:   docs/value-proposition/value-proposition.md

--- app.py
+++ app.py
@@ -4,4 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return "Hallo! Das ist die Startseite der Fitness-Tracker-Webapp."
+
+if __name__ == '__main__':
+    app.run(debug=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    workouts = db.relationship('Workout', backref='user', lazy=True)
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    workout_type = db.Column(db.String(80), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    calories_burned = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
