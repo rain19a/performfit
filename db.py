5af30c85967149b514622f2fbcb01c79abbae77c
Kubijessa <kubilay.jessa@gmail.com>
2024-02-07 23:57:01 +0100

    modified:   db.py 	modified:   templates/index.html

--- db.py
+++ db.py
@@ -21,7 +21,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
-
+    gewicht = db.Column(db.Float)
+    zielgewicht = db.Column(db.Float)
+    
     # Methode zum Setzen des Passwort-Hashes
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
