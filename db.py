05502d5b02730bd8a9042337b8d6277f4c1f1b74
Kubijessa <kubilay.jessa@gmail.com>
2024-02-08 15:34:00 +0100

    modified:   db.py 	new file:   migrations/versions/1d60bc7616d5_deleted_workout_models.py 	new file:   migrations/versions/f5524139c376_added_workout_models.py 	modified:   templates/workout-plan

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash
 from flask import Flask
 from flask_login import UserMixin
 from flask_migrate import Migrate
-
+from datetime import date
 
 # Flask-Anwendung erstellen
 app = Flask(__name__)
@@ -38,10 +38,6 @@ class User(UserMixin, db.Model):
 with app.app_context():
     db.create_all()
 
-# Fügen Sie dies zum db.py hininzu
-
-from datetime import date
-
 class Progress(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -52,6 +48,19 @@ class Progress(db.Model):
     def __repr__(self):
         return f'<Progress {self.user_id} {self.date} Slept: {self.slept_well} Workout: {self.workout_completed}>'
 
-# Nach der Definition des Modells, erneut db.create_all() aufrufen
-with app.app_context():
-    db.create_all()
+class WorkoutDay(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    day_name = db.Column(db.String(50), nullable=False)
+    user = db.relationship('User', backref=db.backref('workout_days', lazy=True))
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    workout_day_id = db.Column(db.Integer, db.ForeignKey('workout_day.id'), nullable=False)
+    exercise_name = db.Column(db.String(100), nullable=False)
+    sets = db.Column(db.Integer, nullable=False)
+    repetitions = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    workout_day = db.relationship('WorkoutDay', backref=db.backref('workouts', lazy=True))
+
+
